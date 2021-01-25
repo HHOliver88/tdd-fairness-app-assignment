a8858eb37cc172b1c98f4735d164109fa57554d8
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-01-25 08:23:41 +0800

    Provided sample test code and sample function.

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 This simple app will just be a single function that accepts 2 values. Build this app in TDD approach.
 **You must write the test first, have it failed, then write the necessary code for it to pass**
 
-1. This function must only accept 2 values
-2. It will return a success message if 2 values (must be float) are equal
+1. This function must only accept 2 arguments (should be not okay if function is passed with a single argument or more than 2 arguments)
+2. It will return a success message if 2 values are equal
 3. It will return a fail message if 2 values are not equal
-4. It will return an error message if 2 values are not floats.
+4. It will return an error message if 2 values are not numbers.
 
 # Important
 
--- test/fair.test.js
+++ test/fair.test.js
@@ -1,7 +1,27 @@
+process.env.NODE_ENV = 'test';
+
 const chai                  = require('chai');
+const expect                = chai.expect;
 
+class Fairness {
+    is_fair(a, b) {
 
-// Create the fairness app function here
+    }
+}
 
+describe("Fairness App", function() {
+    it("it should only accept 2 arguments", function(){
+        let fairness = new Fairness();
+        let a = 1;
+        let b = 2;
+        let c = 3;
+          
+        expect(fairness.is_fair(a)).to.not.be.ok;
 
-// Write your test cases here
+        expect(fairness.is_fair(a, b, c)).to.not.be.ok;
+    });
+
+    it("should return a success message if the two values are equal");
+    it("should return a fail message if 2 values are not equal");
+    it("should return an error message if any of the 2 arguments are not numbers.");
+});
